f70b4c1fd02f0ff01428a234803637da750c7771
kl-vl <191594458+kl-vl@users.noreply.github.com>
2025-09-09 03:23:54 +0300

    Initial Stats module configuration

---
+++ stats/stats-server/src/main/java/ru/practicum/statsserver/error/ErrorHandler.java
@@ -0,0 +1,40 @@
+package ru.practicum.statsserver.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponseDto errorHandlerInternal(final Exception ex, final WebRequest request) {
+
+        log.error("Internal error in {}: {}", request.getDescription(true), ex.getMessage(), ex);
+
+        Map<String, String> details = new HashMap<>();
+        details.put("exception", ex.getClass().getSimpleName());
+        details.put("message", ex.getMessage());
+        details.put("stackTrace", getStackTraceAsString(ex)); // Добавляем stacktrace в details
+
+        return new ErrorResponseDto("Internal server error", "INTERNAL_ERROR", details);
+    }
+
+    private String getStackTraceAsString(Throwable throwable) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        throwable.printStackTrace(printWriter);
+        return stringWriter.toString();
+    }
+
+}
