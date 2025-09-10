316db7938372b68111fcde72fa571d653f5cd2a4
kl-vl <191594458+kl-vl@users.noreply.github.com>
2025-09-10 10:53:36 +0300

    Stat svc client (#4)
    * Stat client web-client pre-implementation
    * Stat web-client on UriComponentsBuilder

---
+++ stats/stats-client/src/main/java/ru/practicum/statsclient/client/ClientRestStatImpl.java
@@ -0,0 +1,67 @@
+package ru.practicum.statsclient.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.practicum.statsdto.EndpointHitDto;
+import ru.practicum.statsdto.ViewStatsDto;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ClientRestStatImpl implements ClientRestStat {
+
+    private final RestClient restClient;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public ClientRestStatImpl(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    @Override
+    public EndpointHitDto addStat(EndpointHitDto dto) {
+        return restClient.post()
+                .uri("/hit")
+                .body(dto)
+                .retrieve()
+                .body(EndpointHitDto.class);
+    }
+
+    @Override
+    public List<ViewStatsDto> getStat(LocalDateTime start, LocalDateTime end, String[] uris, boolean unique) {
+        URI uri = buildStatsUri(start, end, uris, unique);
+
+        ResponseEntity<ViewStatsDto[]> responseEntity = restClient.get()
+                .uri(uri)
+                .retrieve()
+                .toEntity(ViewStatsDto[].class);
+
+        return responseEntity.getBody() != null ? Arrays.asList(responseEntity.getBody()) : Collections.emptyList();
+    }
+
+    private URI buildStatsUri(LocalDateTime start,
+                              LocalDateTime end,
+                              String[] uris,
+                              boolean unique) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/stats")
+                .queryParam("start", formatDateTime(start))
+                .queryParam("end", formatDateTime(end))
+                .queryParam("unique", unique);
+
+        if (uris != null) {
+            for (String uri : uris) {
+                builder.queryParam("uris", uri);
+            }
+        }
+
+        return builder.build().toUri();
+    }
+
+    private String formatDateTime(LocalDateTime dateTime) {
+        return dateTime.format(FORMATTER);
+    }
+}
