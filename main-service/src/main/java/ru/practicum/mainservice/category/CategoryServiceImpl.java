f0ddb64f7e49924be46d04a7ce1a4f67ce62953f
kl-vl <191594458+kl-vl@users.noreply.github.com>
2025-09-15 21:19:10 +0300

    checkstyle. remove unused import

--- main-service/src/main/java/ru/practicum/mainservice/category/CategoryServiceImpl.java
+++ main-service/src/main/java/ru/practicum/mainservice/category/CategoryServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.mainservice.category;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
