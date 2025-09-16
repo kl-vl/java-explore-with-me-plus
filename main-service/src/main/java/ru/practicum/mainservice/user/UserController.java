14a5f33ea828ea4827081c538703801ac48a8e79
Илья <iljiafenix@yandex.ru>
2025-09-16 16:44:46 +0300

    fix: убраны лишние импорты

--- main-service/src/main/java/ru/practicum/mainservice/user/UserController.java
+++ main-service/src/main/java/ru/practicum/mainservice/user/UserController.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
