7b1168307c6452e8608ca0f50b69376a7f5e30d4
Stanislav Emelianov <stasemel@yandex.ru>
2025-09-22 11:51:31 +0300

    Добавлена проверка к CompilationMapper

--- main-service/src/main/java/ru/practicum/mainservice/compilation/CompilationMapper.java
+++ main-service/src/main/java/ru/practicum/mainservice/compilation/CompilationMapper.java
@@ -5,21 +5,22 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.practicum.mainservice.compilation.dto.CompilationCreateDto;
 import ru.practicum.mainservice.compilation.dto.CompilationDto;
+import ru.practicum.mainservice.config.CommonMapperConfiguration;
 import ru.practicum.mainservice.event.EventMapper;
 
 import java.util.HashSet;
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = EventMapper.class)
+@Mapper(componentModel = "spring", uses = EventMapper.class, config = CommonMapperConfiguration.class)
 public interface CompilationMapper {
-    Compilation toEntity(CompilationDto compilationDto);
-
-    @BeforeMapping
+    @BeforeMapping()
     default void validate(Compilation compilation) {
-        if (compilation.getEvents() == null) {
+        if ((compilation != null) && (compilation.getEvents() == null)) {
             compilation.setEvents(new HashSet<>());
         }
     }
+
+    Compilation toEntity(CompilationDto compilationDto);
 
     @Mapping(target = "events", source = "events", ignore = true)
     Compilation toEntity(CompilationCreateDto compilationCreateDto);
